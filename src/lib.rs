34c5c4985cc6a1cc15cf6d3d685bb77142f73e4d
Charlie's Boyfriend <166959314+ReinhardTristanHeydrich@users.noreply.github.com>
2025-10-17 16:32:44 -0300

    Only set rune_arc_active if false

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
-use std::time::Duration;
-
+/*✦════════════════════════════════════════════════════════════ ✦ ═════════════════════════════════════════════════════════════✦*/
+ /*✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ .  Compile-Time ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . */
+/*✦════════════════════════════════════════════════════════════ ✦ ═════════════════════════════════════════════════════════════✦*/
+/*✦════════════════════════ Imports ════════════════════════✦*/
+/*✦───────── From-Softwaer-RS ─────────✦*/
 use eldenring::{
     cs::{CSTaskGroupIndex, CSTaskImp, WorldChrMan},
     fd4::FD4TaskData,
@@ -11,39 +14,60 @@ use eldenring_util::{
     task::CSTaskImpExt,
 };
 
-#[link(name = "kernel32")]
-unsafe extern "C" {
-    unsafe fn DisableThreadLibraryCalls(hmodule: usize) -> bool;
+/*✦───────── Thread ─────────✦*/
+use winapi::um::libloaderapi::DisableThreadLibraryCalls;
+use winapi::um::processthreadsapi::CreateThread;
+use winapi::shared::minwindef::{LPVOID, DWORD};
+use winapi::um::handleapi::CloseHandle;
+use std::ptr;
+
+/*✦───────── Others ─────────✦*/
+use std::time::Duration;
+
+/*✦═════════════════════════ Consts ═════════════════════════✦*/
+const GAME_INJECTION:u32 = 1;
+
+/*✦════════════════════════════════════════════════════════════ ✦ ═════════════════════════════════════════════════════════════✦*/
+ /*✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . ⁺ ✦ Code ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . ⁺ . ✦*/
+/*✦════════════════════════════════════════════════════════════ ✦ ═════════════════════════════════════════════════════════════✦*/
+#[allow(unsafe_op_in_unsafe_fn)]
+unsafe extern "system" fn main(_: LPVOID) -> DWORD {
+    /*✦═════════════════════════════════════════════✦══════════════════════════════════════════════✦*/
+    wait_for_system_init(&Program::current(), Duration::MAX).expect("Could not await system init.");
+    /*✦─────────────────────────────────────────────────────────────────────────────────────────────✦*/
+    let task_system = get_instance::<CSTaskImp>().unwrap().unwrap();
+
+    task_system.run_recurring(|_task_data: &FD4TaskData| {
+        let Some(main_player) = get_instance::<WorldChrMan>()
+            .expect("No reflection data for WorldChrMan")
+            .and_then(|wcm| wcm.main_player.as_mut()) else {
+            return;
+        };
+
+        if !main_player.player_game_data.rune_arc_active {
+            main_player.player_game_data.rune_arc_active = true;
+        }
+    }, CSTaskGroupIndex::FrameBegin);
+    /*✦═════════════════════════════════════════════✦══════════════════════════════════════════════✦*/
+    0
 }
 
 
+
+/*✦════════════════════════════════════════════════════════════ ✦ ═════════════════════════════════════════════════════════════✦*/
+/*✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . ⁺ Dll-Main ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . 　⁺ 　 . ✦ . ⁺ . ✦*/
+/*✦════════════════════════════════════════════════════════════ ✦ ═════════════════════════════════════════════════════════════✦*/
 #[unsafe(no_mangle)]
 #[allow(unsafe_op_in_unsafe_fn)]
-pub unsafe extern "C" fn DllMain(hmodule: usize, reason: u32) -> bool {
-    const GAME_INJECTION:u32 = 1;
+unsafe extern "C" fn DllMain(hmodule: usize, reason: u32) -> bool {
     if reason != GAME_INJECTION {return true;}
-    DisableThreadLibraryCalls(hmodule);
-    
+    DisableThreadLibraryCalls(hmodule as *mut _);
 
-    std::thread::spawn(|| {
-        wait_for_system_init(&Program::current(), Duration::MAX)
-            .expect("Could not await system init.");
-        //--------------------------------------------------------------
-        let task_system = get_instance::<CSTaskImp>().unwrap().unwrap();
-        //==============================================================
-        task_system.run_recurring(
-            |_task_data: &FD4TaskData| {
-                let Some(main_player) = get_instance::<WorldChrMan>()
-                    .expect("No reflection data for WorldChrMan")
-                    .and_then(|wcm| wcm.main_player.as_mut())
-                else {return;};
+    let thread = CreateThread(ptr::null_mut(), 0, Some(main), ptr::null_mut(), 0, ptr::null_mut());
 
-                main_player.player_game_data.rune_arc_active = true;
-            },
-            CSTaskGroupIndex::ChrIns_PostPhysics,
-        );
-        //==============================================================
-    });
+    if !thread.is_null() {CloseHandle(thread);}
+
+
 
     true
 }
